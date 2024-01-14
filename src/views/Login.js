c7ec34585bcee4116b9764b51d833b5378f38035
Hector Garcia <hectorgarx2@gmail.com>
2024-01-14 15:22:46 -0600

    Log in funcional con firebase

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,13 +1,40 @@
-import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert} from 'react-native'
 import React, { Component } from 'react'
 import Header from '../components/Header';
 import {globalStyles} from '../../globalStyles';
+import { FIREBASE_AUTH } from '../../Firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 export default class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            correo: '',
+            password: '',
+            loading: false,
+            auth: FIREBASE_AUTH,
+        };
+    }
     render() {
-        const clickIniciarSesion= () =>{
-            this.props.navigation.navigate('Publicaciones');
-          }
+        const clickIniciarSesion= async () => {
+            let loading = this.state.loading;
+            loading = true;
+            let nombre = this.state.nombre;
+            let correo = this.state.correo;
+            let password = this.state.password;
+            let auth = this.state.auth;
+            console.log(nombre, correo, password)
+
+            try {
+                const respuesta = await signInWithEmailAndPassword(auth, correo, password);
+                console.log(respuesta);
+                this.props.navigation.navigate('Publicaciones');
+            } catch (error) {
+                Alert.alert(error.message);
+            } finally{
+                loading = false;
+            }
+        }
         return (
             <View>
                 <Header navigation={this.props.navigation} 
@@ -18,19 +45,37 @@ export default class Login extends Component {
                 <View style={globalStyles.form}>
                     <Text style={globalStyles.txtBasic}>Correo Institucional</Text>
                     <View style={globalStyles.input}>
-                        <TextInput style={globalStyles.txtInput}></TextInput>
+                        <TextInput 
+                            style={globalStyles.txtInput}
+                            onChangeText={correo =>
+                                this.setState({correo})}
+                            placeholder='Email'
+                            autoCapitalize='none'
+                            inputMode='email'
+                        ></TextInput>
                     </View>
         
                     <Text style={globalStyles.txtBasic}>Contraseña</Text>
                     <View style={globalStyles.input}>
-                        <TextInput style={globalStyles.txtInput}></TextInput>
+                        <TextInput 
+                            style={globalStyles.txtInput}
+                            onChangeText={password =>
+                                this.setState({password})}
+                            placeholder='Password'
+                            secureTextEntry={true}
+                        ></TextInput>
                     </View>
-        
-                    <TouchableOpacity onPress={clickIniciarSesion}>
-                        <View style={globalStyles.boton}>
-                            <Text style={globalStyles.txtBoton}>Iniciar Sesión</Text>
-                        </View>
-                    </TouchableOpacity>
+
+                    {   // Codigo de carga para esperar respuesta del servidor
+                        this.state.loading ? (<ActivityIndicator size={'large'} color={'#33BD78'}/>) :
+                        (
+                            <TouchableOpacity onPress={clickIniciarSesion}>
+                                <View style={globalStyles.boton}>
+                                    <Text style={globalStyles.txtBoton}>Iniciar Sesión</Text>
+                                </View>
+                            </TouchableOpacity>
+                        )
+                    }
         
                 </View>
 
